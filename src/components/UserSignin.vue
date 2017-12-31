38c331c244a21dd0b1f4c6c83a20094614ffd0a4
Eko Wahyudin <eko.wahyudin@yahoo.co.id>
2017-12-31 17:21:55 +0700

    Add user Signup/in

---
+++ src/components/UserSignin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {msg}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserSignin',
+  data () {
+    return {
+      msg: 'User Signin'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/UserSignup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {msg}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserSignup',
+  data () {
+    return {
+      msg: 'User Signup'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
