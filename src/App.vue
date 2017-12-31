67cdaae45f2bb12002ac9f8641ef4a7dcb9dd728
Eko Wahyudin <eko.wahyudin@yahoo.co.id>
2017-12-31 20:25:07 +0700

    fix navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
   <v-app>
-    <navigation-drawer/>
+    <navigation-drawer :open="drawer"/>
 
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-spacer></v-spacer>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+    <v-toolbar fixed app :clipped-left="true">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
+      <v-spacer/>
+      <v-toolbar-title v-text="title"/>
+      <v-spacer/>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
     </v-toolbar>
     <v-content>
       <v-container fluid>
         <v-slide-y-transition mode="out-in">
           <v-layout column align-center>
-            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
+            <router-view/>
           </v-layout>
         </v-slide-y-transition>
       </v-container>
@@ -35,10 +25,12 @@
 </template>
 
 <script>
-  import { NavigationDrawer } from "@/components/common/NavigationDrawer";
+  import NavigationDrawer from '@/components/common/NavigationDrawer.vue'
   export default {
     data () {
       return {
+        drawer: false,
+        fixed: true,
         title: 'Niaga Baru'
       }
     },
--- src/components/common/NavigationDrawer.vue
+++ src/components/common/NavigationDrawer.vue
@@ -1,21 +1,12 @@
 <template>
-  <v-navigation-drawer
-    :fixed="fixed"
-    :clipped="clipped"
-    v-model="drawer"
-    app
-  >
+  <v-navigation-drawer app fixed clipped :value="open">
     <v-list>
-      <v-list-tile 
-        value="true"
-        v-for="(item, i) in items"
-        :key="i"
-      >
+      <v-list-tile value="true" v-for="(item, i) in items" :key="i">
         <v-list-tile-action>
           <v-icon v-html="item.icon"></v-icon>
         </v-list-tile-action>
         <v-list-tile-content>
-          <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          <v-list-tile-title v-text="item.title"/>
         </v-list-tile-content>
       </v-list-tile>
     </v-list>
@@ -27,15 +18,15 @@ export default {
   name: 'NavigationDrawer',
   data () {
     return {
-      drawer: false,
-      fixed: true,
-      clipped: true,
       items: [{
         icon: 'bubble_chart',
         title: 'Inspire'
-      }],
+      }]
     }
-  }
+  },
+  computed: {
+  },
+  props: ['open']
 }
 </script>
 
