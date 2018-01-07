f6a0c798f918b10919879181772fb41da2d75c5e
Eko Wahyudin <eko.wahyudin@yahoo.co.id>
2018-01-07 19:28:38 +0700

    fix auth engine

--- src/engine/auth.js
+++ src/engine/auth.js
@@ -15,21 +15,42 @@ export default {
     }
   },
 
-  async createUserWithEmailAndPassword (email, password, realName) {
-    try {
-      let xxx = await auth.createUserWithEmailAndPassword(email, password)
-      console.log(xxx)
-      user = auth.currentUser
-      if (realName) {
-        let yyy = await user.updateProfile({
-          displayName: realName
-        })
-        console.log(yyy)
+  /**
+   * return promise
+   * @param {*} email
+   * @param {*} password
+   * @param {*} realName
+   */
+  createUserWithEmailAndPassword (email, password, realName) {
+    return new Promise((resolve, reject) => {
+      let warning = []
+      let jobCounter = 1 // 1 for sendEmailVerification
+
+      function checkResolve () {
+        jobCounter--
+        if (jobCounter <= 0) {
+          resolve({status: 0, message: 'Account Created', warning: warning})
+        }
       }
-      await user.sendEmailVerification()
-    } catch (error) {
-      return {code: error.code, msg: error.message}
-    }
-    return {code: 0, msg: 'OK'}
+
+      auth.createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        let user = auth.currentUser
+
+        // send email verification
+        user.sendEmailVerification()
+        .catch((e) => warning.push(e))
+        .done(checkResolve)
+
+        // update display name
+        if (realName) {
+          jobCounter++
+          user.updateProfile({displayName: realName})
+          .catch((e) => warning.push(e))
+          .done(checkResolve)
+        }
+      })
+      .catch((e) => reject(e))
+    })
   }
 }
